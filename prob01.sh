37a599ac12529c375385d4b100c38867492f81c9
sphoujdar <sm.phoujdar@gmail.com>
2020-11-09 13:43:25 +0530

    Add : Read 3 input from user

--- prob01.sh
+++ prob01.sh
@@ -1,1 +1,6 @@
 #!/bin/bash -x
+
+read -p "Please enter 3 Numbers as input separated by a space before hitting enter : " number1 number2 number3
+echo Number 1 : $number1 --- Number 2 : $number2 ---  Number 3 : $number3
+
+
