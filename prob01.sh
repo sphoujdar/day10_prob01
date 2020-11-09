7c9c36952603ccd030d1b7810a3b7bcf79a78954
sphoujdar <sm.phoujdar@gmail.com>
2020-11-09 14:01:54 +0530

    Add : Code to print computation 1 - Corrected

--- prob01.sh
+++ prob01.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -x
 
 read -p "Please enter 3 Numbers as input separated by a space before hitting enter : " number1 number2 number3
-echo Number 1 (a) : $number1 --- Number 2 (b) : $number2 ---  Number 3 (c) : $number3
+echo Number 1 \(a\) : $number1 --- Number 2 \(b\) : $number2 ---  Number 3 \(c\) : $number3
 
 
 #Using BODMASS Rule for all calculations
 solution1=$(($number1+$(($number2*$number3))))
-echo a + b * c = $solution1
+problem1='a + b * c = '
+echo "$problem1$solution1"
 
