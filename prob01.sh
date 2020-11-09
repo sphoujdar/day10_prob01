f2f4d90d81cae6c52fdeaa2ac6a2848a140ff1fa
sphoujdar <sm.phoujdar@gmail.com>
2020-11-09 13:52:04 +0530

    Add : Compution 1 Logic

--- prob01.sh
+++ prob01.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -x
 
 read -p "Please enter 3 Numbers as input separated by a space before hitting enter : " number1 number2 number3
-echo Number 1 : $number1 --- Number 2 : $number2 ---  Number 3 : $number3
+echo Number 1 (a) : $number1 --- Number 2 (b) : $number2 ---  Number 3 (c) : $number3
 
+
+#Using BODMASS Rule for all calculations
+solution1=$(($number1+$(($number2*$number3))))
+echo a + b * c = $solution1
 
